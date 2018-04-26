007f25dc1c193ea87fbd2b5efa7ec2219c58c3ee
Syntruth <syntruth@gmail.com>
2018-04-26 16:42:11 -0600

    Refactor to DRY things up

--- dice.erl
+++ dice.erl
@@ -29,38 +29,38 @@ get_tally(Num, Sides) ->
   [random:uniform(Sides) | get_tally(Num - 1, Sides)].
 
 
-d4() -> d4(0).
-d4(Mod) -> roll(1, 4, Mod).
+d4()         -> d4(0).
+d4(Mod)      -> d4(1, Mod).
 d4(Num, Mod) -> roll(Num, 4, Mod).
 
 
-d6() -> d6(0).
-d6(Mod) -> roll(1, 6, Mod).
+d6()         -> d6(0).
+d6(Mod)      -> d6(1, Mod).
 d6(Num, Mod) -> roll(Num, 6, Mod).
 
 
-d8() -> d8(0).
-d8(Mod) -> roll(1, 8, Mod).
+d8()         -> d8(0).
+d8(Mod)      -> d8(1, Mod).
 d8(Num, Mod) -> roll(Num, 8, Mod).
 
 
-d10() -> d10(0).
-d10(Mod) -> roll(1, 10, Mod).
+d10()         -> d10(0).
+d10(Mod)      -> d10(1, Mod).
 d10(Num, Mod) -> roll(Num, 10, Mod).
 
 
-d12() -> d12(0).
-d12(Mod) -> roll(1, 12, Mod).
+d12()         -> d12(0).
+d12(Mod)      -> d12(1, Mod).
 d12(Num, Mod) -> roll(Num, 12, Mod).
 
 
-d20() -> d20(0).
-d20(Mod) -> roll(1, 20, Mod).
+d20()         -> d20(0).
+d20(Mod)      -> d20(1, Mod).
 d20(Num, Mod) -> roll(Num, 20, Mod).
 
 
-d100() -> d100(0).
-d100(Mod) -> roll(1, 100, Mod).
+d100()         -> d100(0).
+d100(Mod)      -> d100(1, Mod).
 d100(Num, Mod) -> roll(Num, 100, Mod).
 
 
